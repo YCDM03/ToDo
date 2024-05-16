61939776252d21345487d7c8503da688a58b628c
YCDM03 <ycdm03@gmail.com>
2024-05-16 11:57:59 +0900

    refactor: add a key in each ul.

--- src/App.jsx
+++ src/App.jsx
@@ -94,6 +94,7 @@ function App() {
       </form>
       <h3> Working...</h3>
       <ul
+        key="working"
         style={{
           width: "100%",
           listStyle: "none",
@@ -109,14 +110,13 @@ function App() {
           })}
           deleteTodo={deleteTodo}
           changeDone={changeDone}
-        >
-          {" "}
-        </MakeList>
+        />
       </ul>
 
       <h3> Done..!</h3>
 
       <ul
+        key="done"
         style={{
           width: "100%",
           listStyle: "none",
@@ -141,40 +141,38 @@ function App() {
 function MakeList({ list, deleteTodo, changeDone }) {
   return list.map(({ id, title, body, isDone }) => {
     return (
-      <>
-        <li
-          style={{
-            width: "20%",
-            textAlign: "center",
-            border: "2px solid white",
-            margin: "10px",
-            display: "flex",
-            flexDirection: "column",
-          }}
-          key={"list" + id}
-        >
-          <h3 style={{ marginTop: "5px" }}>{title}</h3>
-          <p>{body}</p>
-          <div style={{ width: "100%", marginTop: "auto" }}>
-            <button
-              style={{ width: "50%" }}
-              onClick={() => {
-                deleteTodo(id);
-              }}
-            >
-              삭제하기
-            </button>
-            <button
-              style={{ width: "50%" }}
-              onClick={() => {
-                changeDone(id, title, body, isDone);
-              }}
-            >
-              {isDone ? "취소" : "완료"}
-            </button>
-          </div>
-        </li>
-      </>
+      <li
+        style={{
+          width: "20%",
+          textAlign: "center",
+          border: "2px solid white",
+          margin: "10px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+        key={"list" + id}
+      >
+        <h3 style={{ marginTop: "5px" }}>{title}</h3>
+        <p>{body}</p>
+        <div style={{ width: "100%", marginTop: "auto" }}>
+          <button
+            style={{ width: "50%" }}
+            onClick={() => {
+              deleteTodo(id);
+            }}
+          >
+            삭제하기
+          </button>
+          <button
+            style={{ width: "50%" }}
+            onClick={() => {
+              changeDone(id, title, body, isDone);
+            }}
+          >
+            {isDone ? "취소" : "완료"}
+          </button>
+        </div>
+      </li>
     );
   });
 }
